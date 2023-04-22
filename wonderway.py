8fb8f52bb53671e3d958ea1ff2d7bc8cebe7d32c
Anthony Nuon-Prak <anuonprak@gmail.com>
2023-04-22 00:43:03 -0700

    Basic implementation of user interests

--- wonderway.py
+++ wonderway.py
@@ -1,10 +1,19 @@
 ASK_LOCATION_START = 'What is your current location: '
 ASK_LOCATION_END = 'What is your destination: '
+ASK_INTERESTS = 'What interests do you want to see?'
+WONDERS = ['concert', 'restaurant']
 
 def askUserInput(prompt: str) -> str:
     return input(prompt)
 
+def findLocationSuggestions():
+    pass
 
+def askUserInterests() -> str:
+    print(ASK_INTERESTS)
+    for idx, itm in enumerate(WONDERS, 1):
+        print(f'{idx}. {itm}')
+    return askUserInput('')
 
 if __name__ == '__main__':
     print(askUserInput(ASK_LOCATION_START))
